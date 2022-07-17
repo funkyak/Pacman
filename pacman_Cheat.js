3d3dd7578adbaa07a103f00263d4f7248ce9d44b
funkyak <34272259+funkyak@users.noreply.github.com>
2022-07-17 21:31:16 +0100

    Add files via upload
    Pacman Canvas is Open Source, written by platzh1rsch. You can get the code on github. 
    https://github.com/platzhersh/pacman-canvas

---
+++ pacman_Cheat.js
@@ -0,0 +1,28 @@
+// Needs to be completed in breakout
+window.pacman = pacman
+
+// Needs to be complete when game is running 
+// infinte lives
+
+function infintelives(){if(pacman.lives <3){pacman.lives = 3}}
+var bot = setInterval(infintelives,100)// 100 ms (Time)
+
+// Needs to be complete when game is running 
+// Invincibility 
+pacman.die =function(){} // Calls to an empty function 
+
+
+// enable powerpill \\
+// Attacking the ghosts for this version 
+
+window.inky = inky
+window.pinky = pinky 
+window.blinky = blinky
+window.clyde = clyde
+// Needs to be complete when game is running 
+function powerup (){
+inky.dazzle();
+pinky.dazzle();
+blinky.dazzle();
+clyde.dazzle();}
+var bot = setInterval(powerup,100); // 100 ms (Time)
